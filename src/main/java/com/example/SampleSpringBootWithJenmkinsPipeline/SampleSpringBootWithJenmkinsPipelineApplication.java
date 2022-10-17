5cfe2613bb467b352405d6514f4afde48aed6df8
chandrakantparadkar <chandrakant.paradkar@gmail.com>
2022-10-17 18:42:58 +0530

    initial commit with code

---
+++ src/main/java/com/example/SampleSpringBootWithJenmkinsPipeline/SampleSpringBootWithJenmkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.SampleSpringBootWithJenmkinsPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootWithJenmkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootWithJenmkinsPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SampleSpringBootWithJenmkinsPipeline/SampleSpringBootWithJenmkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SampleSpringBootWithJenmkinsPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootWithJenmkinsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
